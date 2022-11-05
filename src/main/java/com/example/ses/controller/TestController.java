7d94758abb6c5f14e2086b912be46950b3a0f91b
tuyenbd <tuyenbd@tcbs.com.vn>
2022-11-05 15:31:54 +0700

    Expose metrics

--- src/main/java/com/example/ses/controller/TestController.java
+++ src/main/java/com/example/ses/controller/TestController.java
@@ -1,16 +1,20 @@
 package com.example.ses.controller;
 
 import com.example.ses.socket.SesServer;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class TestController {
+
+    private final SesServer sesServer;
 
     @RequestMapping("/test")
     public String test(@RequestParam int messageNumberPerSecond, @RequestParam int times) throws Exception {
-        SesServer.sendRequest(messageNumberPerSecond, times);
+        sesServer.sendRequest(messageNumberPerSecond, times);
         return "Ok";
     }
 }
--- src/main/java/com/example/ses/socket/SesServer.java
+++ src/main/java/com/example/ses/socket/SesServer.java
@@ -1,6 +1,8 @@
 package com.example.ses.socket;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
@@ -8,10 +10,12 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -26,6 +30,18 @@ public class SesServer {
 
     @Value("${app.port}")
     private int port;
+
+    @Autowired
+    private MeterRegistry registry;
+
+    private AtomicInteger sentMessageCount = new AtomicInteger(0);
+    private AtomicInteger sentMessageTime = new AtomicInteger(0);
+
+    @PostConstruct
+    public void init() {
+        sentMessageCount = registry.gauge( "sent_message_count", new AtomicInteger(0));
+        sentMessageTime = registry.gauge( "sent_message_time", new AtomicInteger(0));
+    }
 
     @EventListener(ApplicationReadyEvent.class)
     public void startServer() throws Exception {
@@ -56,18 +72,25 @@ public class SesServer {
 //        log.info("Message Received: " + message);
     }
 
-    public static void sendRequest(int messageNumberPerSecond, int times) throws Exception {
+    public void sendRequest(int messageNumberPerSecond, int times) throws Exception {
         log.info("Start send messageNumberPerSecond {} times {}", messageNumberPerSecond, times);
+        outputStream.writeObject("start");
+        long startTime = System.currentTimeMillis();
+        sentMessageTime.set(0);
+        sentMessageCount.set(0);
         for (int i = 0; i < times; i++) {
             long start = System.currentTimeMillis();
             for (int j = 0; j < messageNumberPerSecond; j++) {
                 outputStream.writeObject(generateMessage(j));
+                sentMessageCount.incrementAndGet();
             }
             long delay = 1000 - (System.currentTimeMillis() - start);
             if (delay > 0) {
                 Thread.sleep(delay);
             }
         }
+        outputStream.writeObject("end");
+        sentMessageTime.set((int) (System.currentTimeMillis() - startTime));
         log.info("Send done");
     }
 
